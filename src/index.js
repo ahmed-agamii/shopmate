4a2a33afde4dd7b808b73638b25e28b69e914935
ahmed-agamii <ahmed.kamel01124@gmail.com>
2024-09-03 18:37:13 +0300

    Removed merge conflict markers and fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-<<<<<<< HEAD
 import { CartProvider } from "./context/CartContext";
-=======
->>>>>>> 1ab8b41185ff15b9d49295a8eee60d03a5cb2fb0
 import "./index.css";
 import App from "./App";
 
@@ -12,13 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-<<<<<<< HEAD
       <CartProvider>
         <App />
       </CartProvider>
-=======
-      <App />
->>>>>>> 1ab8b41185ff15b9d49295a8eee60d03a5cb2fb0
     </Router>
   </React.StrictMode>
 );
